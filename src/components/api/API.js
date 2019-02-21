87d4edfa62dc48aa185e0a7771b6c794d1fc8b89
Fajar Gilang Bima Pamungkas <fajar.gilang@smart-it.co.id>
2019-02-21 18:42:50 +0700

    tes

--- src/components/api/API.js
+++ src/components/api/API.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 import { AsyncStorage, Platform } from 'react-native';
 class API {
-    constructor() {
-      this.api = axios.create({
-        baseURL: 'http://124.81.225.71:5757/tes_ando/api/',        
-        // baseURL: 'http://124.81.225.71:5757/tes_ando/api/login.php', //PATH Login
-        // baseURL: 'http://124.81.225.71:5757/tes_ando/api/signup.php', //PATH Sign UP
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      });
-    }
-    getDeviceType() {
-        return this.api.get('/devices/type');
+  constructor() {
+    this.api = axios.create({
+      baseURL: 'http://apps.merakjaya.co.id/MJBBA/ando/main/',
+      // baseURL: 'http://124.81.225.71:5757/tes_ando/api/login.php', //PATH Login
+      // baseURL: 'http://124.81.225.71:5757/tes_ando/api/signup.php', //PATH Sign UP
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       }
+    });
+  }
+
+  login(email, password) {
+    console.log('jalan gan', email);
+    return this.api.post('/login', {
+      email: email,
+      password: password,
+    });
+  }
 }
--- src/components/screens/LoginScreen.js
+++ src/components/screens/LoginScreen.js
@@ -1,22 +1,69 @@
 import React, { Component } from 'react';
-import { View, ImageBackground, StyleSheet, Image, TextInput
-,TouchableOpacity , Alert , ScrollView } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import {
+    View, ImageBackground, StyleSheet, Image, TextInput
+    , TouchableOpacity, Alert, ScrollView
+} from 'react-native';
 import { Content, Text, Form, Item, Label, Input, Button, Spinner } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
-class LoginScreen extends Component{
-	constructor(props){
-		super(props);
+import API from '../api/API';
 
-		this.state = {email:'',password:''}
-	}
+class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' }
+    }
 
-	render(){
-		return(
-			// <Text style={styles.text1}>Lupa password111 ? </Text>
-      <Text style={styles.text1}>Lupa password ? </Text>
-		)
-	}
+    onEmailChange(text) {
+        this.setState({ email: text })
+    }
+
+    onPasswordChange(text) {
+        this.setState({ password: text })
+    }
+
+    onButtonPress() {
+        let api = new API();
+        api.login(email, password)
+            .then((response) => Alert.alert('berhasil'))
+            .catch((error) => Alert.alert('gagal'));
+    }
+
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <Form style={[{ width: '100%' }]}>
+                    <Item stackedLabel>
+                        <Label style={{ padding: 0, margin: 0 }}>Your E-mail</Label>
+                        <Input
+                            keyboardType="email-address"
+                            style={[{ paddingTop: 2, paddingBottom: 2 }]}
+                            onChangeText={this.onEmailChange.bind(this)}
+                            value={this.state.email}
+                        />
+                    </Item>
+                    <Item stackedLabel>
+                        <Label>Password</Label>
+                        <Input
+                            style={[{ paddingTop: 2, paddingBottom: 2 }]}
+                            secureTextEntry
+                            onChangeText={this.onPasswordChange.bind(this)}
+                            value={this.state.password}
+                        />
+                    </Item>
+                </Form>
+
+                <View style={[{ flex: 1, flexDirection: 'row' }]}>
+                    <View style={{ flex: 1 }}>
+                        <Button block rounded style={{ elevation: 0 }} onPress={this.onButtonPress.bind(this)}>
+                            <Text>Login</Text>
+                        </Button>
+                    </View>
+                </View>
+                <Text style={styles.text1}>Lupa password ? </Text>
+            </View>
+        )
+    }
 }
 const styles = StyleSheet.create({
     MainContainer: {
@@ -26,13 +73,13 @@ const styles = StyleSheet.create({
     TextInputStyleClass: {
         borderColor: '#2196F3',
         backgroundColor: 'white',
-        textAlign: 'center',        
-        width:200,
-        borderRadius:5,
+        textAlign: 'center',
+        width: 200,
+        borderRadius: 5,
         fontSize: 15,
-        marginBottom:20,    
-        borderWidth:0.5,
-        height:40,
+        marginBottom: 20,
+        borderWidth: 0.5,
+        height: 40,
     },
     SectionStyle: {
         flexDirection: 'row',
@@ -56,28 +103,28 @@ const styles = StyleSheet.create({
     text: {
         textAlign: 'center',
         color: 'white',
-        marginTop:70,
+        marginTop: 70,
         backgroundColor: 'rgba(0,0,0,0)',
         fontSize: 15
     },
     btnsignup: {
         textAlign: 'center',
         color: 'white',
-        marginTop:60,
+        marginTop: 60,
         // backgroundColor: '#fff',
         fontSize: 15,
-        textDecorationLine:'underline'
+        textDecorationLine: 'underline'
     },
     text1: {
         textAlign: 'center',
         color: 'white',
-        marginTop:5,
+        marginTop: 5,
         backgroundColor: 'rgba(0,0,0,0)',
         fontSize: 15
     },
-    underline:{
-        textDecorationLine:'underline'
+    underline: {
+        textDecorationLine: 'underline'
     }
 });
 
-export{LoginScreen}
+export { LoginScreen }
